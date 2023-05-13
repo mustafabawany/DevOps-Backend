6f1bee12f645d1d1f95d1e5f7884b61efe4dd56b
mustafabawany <k191273@nu.edu.pk>
2023-05-13 19:26:54 +0500

    package.json edited

--- test/selenium/post-creation.js
+++ test/selenium/post-creation.js
@@ -2,7 +2,7 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
 const assert = require('assert');
 
 (async function example() {
-  let driver = await new Builder().forBrowser('MicrosoftEdge').build();
+  let driver = await new Builder().forBrowser('chrome').build();
   try {
     await driver.get('http://localhost:4000/compose');
 
